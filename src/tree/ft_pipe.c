9af1fa124c6140f39813a36efcb6849b3381c381
brendon <brendonsilva.santos@gmail.com>
2025-03-13 00:43:44 +0000

    pipe com um lado nulo

--- src/tree/ft_pipe.c
+++ src/tree/ft_pipe.c
@@ -6,7 +6,7 @@
 /*   By: breda-si <breda-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 10:02:12 by breda-si          #+#    #+#             */
-/*   Updated: 2025/03/10 10:02:14 by breda-si         ###   ########.fr       */
+/*   Updated: 2025/03/13 00:36:45 by breda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,23 @@ void	*ft_pipe(t_token *start, t_token *aux, t_minishell *bash)
 	t_token	*left;
 	t_token	*right;
 
-	if (!aux || !aux->next)
+	if (!aux)
 		return (NULL);
 	pipe = malloc(sizeof(t_pipe));
 	if (!pipe)
 		return (NULL);
-	left = start;
-	right = aux->next;
+	left = NULL;
+	right = NULL;
 	if (aux->prev)
+	{
+		left = start;
 		aux->prev->next = NULL;
+	}
 	if (aux->next)
+	{
+		right = aux->next;
 		aux->next->prev = NULL;
+	}
 	free_token(aux);
 	pipe->type = PIPE;
 	pipe->left = ft_tree(left, bash);
