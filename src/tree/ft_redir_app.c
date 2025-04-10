9aad56909eb6e365214e666c8c1c218ecf1028ac
brendon <brendonsilva.santos@gmail.com>
2025-04-10 17:31:47 +0100

    ajuste todos redirs

--- src/tree/ft_redir_app.c
+++ src/tree/ft_redir_app.c
@@ -6,7 +6,7 @@
 /*   By: breda-si <breda-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 10:02:26 by breda-si          #+#    #+#             */
-/*   Updated: 2025/04/05 14:35:27 by breda-si         ###   ########.fr       */
+/*   Updated: 2025/04/10 01:19:33 by breda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,10 @@ void	*ft_redir_app(t_token *start, t_token *aux, t_minishell *bash)
 	t_redir	*redir;
 
 	if (!aux || ft_ambiguous_redir(aux, bash))
-		return (free_null_redir(aux));
+	{
+		free_token_list(start);
+		return (NULL);
+	}
 	redir = ft_init_redir(REDIR_APP, aux);
 	if (!redir)
 		return (free_null_redir(aux));
--- src/tree/ft_redir_in.c
+++ src/tree/ft_redir_in.c
@@ -6,7 +6,7 @@
 /*   By: breda-si <breda-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 10:02:22 by breda-si          #+#    #+#             */
-/*   Updated: 2025/04/05 14:34:36 by breda-si         ###   ########.fr       */
+/*   Updated: 2025/04/10 01:19:23 by breda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,11 @@
 void	*ft_redir_in(t_token *start, t_token *aux, t_minishell *bash)
 {
 	t_redir	*redir;
-
 	if (!aux || ft_ambiguous_redir(aux, bash))
-		return (free_null_redir(aux));
+	{
+		free_token_list(start);
+		return (NULL);
+	}
 	redir = ft_init_redir(REDIR_IN, aux);
 	if (!redir)
 		return (free_null_redir(aux));
